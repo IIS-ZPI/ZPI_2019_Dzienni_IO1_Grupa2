ba4076b680bbac63a6642588695c94c3d3cc0f87
Hubert Liberadzki <209377@edu.p.lodz.pl>
2019-05-15 15:28:13 +0200

    Added multiplication interface

--- src/main/java/io1_grupa2/Math.java
+++ src/main/java/io1_grupa2/Math.java
@@ -1,6 +1,6 @@
 package main.java.io1_grupa2;
 
-public class Math implements IArithmeticAdd
+public class Math implements IArithmeticAdd, IArithmeticsMult
 {
     public double Addition(double A, double B)
     {
