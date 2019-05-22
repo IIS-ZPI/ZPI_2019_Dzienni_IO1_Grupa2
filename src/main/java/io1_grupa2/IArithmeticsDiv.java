40d16f945eaad00e758ddd3186d24bb6c2e6d80b
Pelekon <pelekon@gmail.com>
2019-05-22 15:30:04 +0200

    Delete old project file.

--- src/main/java/io1_grupa2/IArithmeticsDiv.java
+++
@@ -1,8 +0,0 @@
-package main.java.io1_grupa2;
-
-//interface with methodes to divide
-public interface IArithmeticsDiv {
-    // methode providing division
-    double division(double a, double b);
-
-}
--- src/main/java/io1_grupa2/IArithmeticsMult.java
+++
@@ -1,8 +0,0 @@
-package main.java.io1_grupa2;
-
-// interface which provides method
-public interface IArithmeticsMult
-{
-    //multiplication method
-    double multiplication(double A, double B);
-}
