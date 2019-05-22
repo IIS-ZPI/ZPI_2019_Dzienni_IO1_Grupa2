d4abc3a9b1dcbba6ed92833968d150ebd536fe6a
Kamil Ostrowski <ostrowskikamil04@gmail.com>
2019-05-22 14:56:42 +0200

    Revert "Added comment for 6.0."

--- src/main/java/io1_grupa2/IArithmeticDiff.java
+++ src/main/java/io1_grupa2/IArithmeticDiff.java
@@ -2,7 +2,6 @@ package main.java.io1_grupa2;
 //Interface for difference
 public interface IArithmeticDiff {
     //Method for difference
-    //Arguments are a and b and those are doubles
     double difference(double a, double b);
 
 }
