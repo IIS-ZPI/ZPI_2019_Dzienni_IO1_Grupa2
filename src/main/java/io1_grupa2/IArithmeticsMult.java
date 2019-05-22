e00a2a2fa931d03b65050787f35175cc555eaa65
Hubert Liberadzki <209377@edu.p.lodz.pl>
2019-05-22 14:31:59 +0200

    zad5_2

--- src/main/java/io1_grupa2/IArithmeticsMult.java
+++ src/main/java/io1_grupa2/IArithmeticsMult.java
@@ -3,5 +3,5 @@ package main.java.io1_grupa2;
 public interface IArithmeticsMult
 {
     //multiplication method
-    double multiplication(double A, double B);
+    double Multiplication(double A, double B);
 }
