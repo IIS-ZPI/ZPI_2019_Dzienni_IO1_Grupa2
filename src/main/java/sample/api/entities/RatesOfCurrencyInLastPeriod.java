70ab7c298a33bee20ef382525327f0d874b61929
Azahil <danio.kow@gmail.com>
2019-06-12 14:02:41 +0200

    Add entity for Rate of Currency in last given period

---
+++ src/main/java/sample/api/entities/RatesOfCurrencyInLastPeriod.java
@@ -0,0 +1,35 @@
+package sample.api.entities;
+
+public class RatesOfCurrencyInLastPeriod {
+    // currency code name
+    private String code;
+    // date of obtained data
+    private String effectiveDate;
+    // rates
+    private double mid;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public double getMid() {
+        return mid;
+    }
+
+    public void setMid(double mid) {
+        this.mid = mid;
+    }
+}
