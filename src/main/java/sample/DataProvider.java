991d96f52de5730ded4e10884c28596af8c11913
Pelekon <pelekon@gmail.com>
2019-06-19 15:09:32 +0200

    Build fix.

--- src/main/java/sample/DataProvider.java
+++ src/main/java/sample/DataProvider.java
@@ -12,5 +12,48 @@ public class DataProvider implements IDataProvider
         apiDataProvider = ApiConnector.GetInstance();
     }
 
-    
+    @Override
+    public int GetSessionIncreaseAmount(PeriodEnum period, String currency) {
+        return 0;
+    }
+
+    @Override
+    public int GetSessionDecreaseAmount(PeriodEnum period, String currency) {
+        return 0;
+    }
+
+    @Override
+    public int GetSessionWithoutChangeAmount(PeriodEnum period, String currency) {
+        return 0;
+    }
+
+    @Override
+    public double GetMedianOfRate(PeriodEnum period, String currency) {
+        return 0;
+    }
+
+    @Override
+    public double GetDominantOfRate(PeriodEnum period, String currency) {
+        return 0;
+    }
+
+    @Override
+    public double GetStandardDevationOfRate(PeriodEnum period, String currency) {
+        return 0;
+    }
+
+    @Override
+    public double GetCoefficientOfVariationOfRate(PeriodEnum period, String currency) {
+        return 0;
+    }
+
+    @Override
+    public void GetMonthlyDistributionOfChanges(String currencyOne, String currencyTwo) {
+
+    }
+
+    @Override
+    public void GetQuarterDistributionOfChanges(String currencyOne, String currencyTwo) {
+
+    }
 }
