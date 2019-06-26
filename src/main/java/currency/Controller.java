14cb2381224815ae552c7cb4d9bfeaa45f00622d
Hubert Liberadzki <209377@edu.p.lodz.pl>
2019-06-26 23:30:01 +0200

    remove conditions and add default values in comboboxes

--- src/main/java/currency/Controller.java
+++ src/main/java/currency/Controller.java
@@ -25,59 +25,46 @@ public class Controller implements Initializable {
 
     IDataProvider provider = DataProviderFactory.getDefaultDataProvider();
 
-    @FXML
-    private ComboBox<String> queriesComboBox;
+    @FXML private ComboBox<String> queriesComboBox;
     String[] queries = {"Ilość sesji wzrostowych, spadkowych i bez zmian",
             "Miary statystyczne(mediana, dominanta, odchylenie standardowe, współczynnik zmienności",
             "Rozkład zmian miesięcznych i kwartalnych dla par walutowych"};
-    @FXML
-    private ComboBox<String> currencyComboBox;
+    @FXML private ComboBox<String> currencyComboBox;
     ObservableList<String> currency = FXCollections.observableArrayList();
 
-    @FXML
-    private ComboBox<String> currencyComboBox2;
+    @FXML private ComboBox<String> currencyComboBox2;
 
-    @FXML
-    private ComboBox<String> periodComboBox;
+    @FXML private ComboBox<String> periodComboBox;
     ObservableList<String> periods = FXCollections.observableArrayList("1 tydzień", "2 tygodnie", "1 miesiąc", "3 miesiące", "6 miesięcy",
             "rok");
 
-    @FXML
-    private Button executeButton;
+    @FXML private Button executeButton;
 
-    @FXML
-    private Button clearListButton;
+    @FXML private Button clearListButton;
 
-    @FXML
-    private Button exitButton;
+    @FXML private Button exitButton;
 
-    @FXML
-    private Button drawChartButton;
+    @FXML private Button drawChartButton;
 
-    @FXML
-    private ListView<String> listOfQueryParameters;
+    @FXML private ListView<String> listOfQueryParameters;
     ObservableList<String> parameters1 = FXCollections.observableArrayList("Sesje wzrostowe", "Sesje spadkowe", "Sesje bez zmian");
     ObservableList<String> parameters2 = FXCollections.observableArrayList("Mediana", "Dominanta", "Odchylenie standardowe",
             "Współczynnik zmienności");
     ObservableList<String> parameters3 = FXCollections.observableArrayList("Rozkład zmian miesięcznych", "Rozklad zmian kwartalnych");
 
-    @FXML
-    private ListView<String> listOfData;
+    @FXML private ListView<String> listOfData;
     String queryValue;
     ObservableList<String> queryValueList = FXCollections.observableArrayList();
 
-    @FXML
-    public void comboBoxListener() {
+    @FXML public void comboBoxListener() {
         setListViewContent(queriesComboBox.getSelectionModel().getSelectedItem());
     }
 
-    @FXML
-    private void exit() {
+    @FXML private void exit() {
         System.exit(0);
     }
 
-    @FXML
-    private void executeQuery() {
+    @FXML private void executeQuery() {
         /*if(!listOfQueryParameters.getSelectionModel().isEmpty() && !currencyComboBox.getSelectionModel().isEmpty() && !periodComboBox.getSelectionModel().isEmpty()) {
             listOfData.getItems().clear();
             queryValue = listOfQueryParameters.getSelectionModel().getSelectedItem()
@@ -104,62 +91,59 @@ public class Controller implements Initializable {
         double resultDouble = 0;
         int resultInt = 0;
         String resultType = null;
-        if (!query.isEmpty() && !currency1.isEmpty() && !periodComboBox.getSelectionModel().getSelectedItem().isEmpty()) {
 
-            listOfData.getItems().clear();
-            clearList();
+        listOfData.getItems().clear();
+        clearList();
 
-            if (query == parameters1.get(0)) {
-                resultInt = provider.getSessionIncreaseAmount(period, currency1);
-                resultType = "int";
-            }
-            if (query == parameters1.get(1)) {
-                resultInt = provider.getSessionDecreaseAmount(period, currency1);
-                resultType = "int";
-            }
-            if (query == parameters1.get(2)) {
-                resultInt = provider.getSessionWithoutChangeAmount(period, currency1);
-                resultType = "int";
-            }
-            if (query == parameters2.get(0)) {
-                resultDouble = provider.getMedianOfRate(period, currency1);
-                resultType = "double";
-            }
-            if (query == parameters2.get(1)) {
-                resultList = provider.getDominantOfRate(period, currency1);
-                resultType = "list";
-            }
-            if (query == parameters2.get(2)) {
-                resultDouble = provider.getStandardDevationOfRate(period, currency1);
-                resultType = "double";
-            }
-            if (query == parameters2.get(3)) {
-                resultDouble = provider.getCoefficientOfVariationOfRate(period, currency1);
-                resultType = "double";
-            }
-            if (!currency2.isEmpty() && query == parameters3.get(0)) {
-                resultList = provider.getMonthlyDistributionOfChanges(currency1, currency2);
-                resultType = "list";
-            }
-            if (!currency2.isEmpty() && query == parameters3.get(1)) {
-                resultList = provider.getQuarterDistributionOfChanges(currency1, currency2);
-                resultType = "list";
-            }
-            if(resultType == "list") {
-                queryValue = query + " / Waluta: " + currency1 + " \nZa okres: " + periodString + "\nWartość:\t\t" + resultList.toString();
-            } else if(resultType == "double"){
-                queryValue = query + " / Waluta: " + currency1 + " \nZa okres: " + periodString + "\nWartość:\t\t" + resultDouble;
-            } else if(resultType == "int"){
-                queryValue = query + " / Waluta: " + currency1 + " \nZa okres: " + periodString + "\nWartość:\t\t" + resultInt;
-            }
-            queryValueList.add(queryValue);
-            listOfData.getItems().addAll(queryValueList);
+        if (query == parameters1.get(0)) {
+            resultInt = provider.getSessionIncreaseAmount(period, currency1);
+            resultType = "int";
         }
+        if (query == parameters1.get(1)) {
+            resultInt = provider.getSessionDecreaseAmount(period, currency1);
+            resultType = "int";
+        }
+        if (query == parameters1.get(2)) {
+            resultInt = provider.getSessionWithoutChangeAmount(period, currency1);
+            resultType = "int";
+        }
+        if (query == parameters2.get(0)) {
+            resultDouble = provider.getMedianOfRate(period, currency1);
+            resultType = "double";
+        }
+        if (query == parameters2.get(1)) {
+            resultList = provider.getDominantOfRate(period, currency1);
+            resultType = "list";
+        }
+        if (query == parameters2.get(2)) {
+            resultDouble = provider.getStandardDevationOfRate(period, currency1);
+            resultType = "double";
+        }
+        if (query == parameters2.get(3)) {
+            resultDouble = provider.getCoefficientOfVariationOfRate(period, currency1);
+            resultType = "double";
+        }
+        if (!currency2.isEmpty() && query == parameters3.get(0)) {
+            resultList = provider.getMonthlyDistributionOfChanges(currency1, currency2);
+            resultType = "list";
+        }
+        if (!currency2.isEmpty() && query == parameters3.get(1)) {
+            resultList = provider.getQuarterDistributionOfChanges(currency1, currency2);
+            resultType = "list";
+        }
+        if (resultType == "list") {
+            queryValue = query + " / Waluta: " + currency1 + " \nZa okres: " + periodString + "\nWartość:\t\t" + resultList.toString();
+        } else if (resultType == "double") {
+            queryValue = query + " / Waluta: " + currency1 + " \nZa okres: " + periodString + "\nWartość:\t\t" + resultDouble;
+        } else if (resultType == "int") {
+            queryValue = query + " / Waluta: " + currency1 + " \nZa okres: " + periodString + "\nWartość:\t\t" + resultInt;
+        }
+        queryValueList.add(queryValue);
+        listOfData.getItems().addAll(queryValueList);
 
     }
 
-    @FXML
-    private void clearList() {
+    @FXML private void clearList() {
         queryValueList.clear();
         listOfData.getItems().clear();
         listOfData.getItems().addAll(queryValueList);
@@ -171,6 +155,9 @@ public class Controller implements Initializable {
         periodComboBox.getItems().addAll(periods);
         currencyComboBox.getItems().addAll(currency);
         currencyComboBox2.getItems().addAll(currency);
+        periodComboBox.getSelectionModel().selectFirst();
+        currencyComboBox.getSelectionModel().select(1);
+        currencyComboBox2.getSelectionModel().select(7);
     }
 
     private void setListViewContent(String query) {
@@ -186,8 +173,7 @@ public class Controller implements Initializable {
             listOfQueryParameters.getItems().addAll(parameters3);
     }
 
-    @FXML
-    private void drawChart() {
+    @FXML private void drawChart() {
         try {
             Parent root1 = FXMLLoader.load(getClass().getResource("..\\line_chart.fxml"));
             Stage stage = new Stage();
@@ -217,10 +203,10 @@ public class Controller implements Initializable {
             return null;
     }
 
-    private void initCurrencyList(){
-        String[] cr = {"THB", "USD","AUD","HKD","CAD","NZD","SGD","EUR","HUF","CHF","GBP","UAH","JPY","CZK",
-                "DKK","NOK","SEK","HRK","RON","BGN","TRY","ILS","CLP","PHP","MXN","ZAR","BRL","MYR",
-                "RUB","IDR","INR","KRW","CNY","XDR"};
+    private void initCurrencyList() {
+        String[] cr = {"THB", "USD", "AUD", "HKD", "CAD", "NZD", "SGD", "EUR", "HUF", "CHF", "GBP", "UAH", "JPY", "CZK", "DKK", "NOK",
+                "SEK", "HRK", "RON", "BGN", "TRY", "ILS", "CLP", "PHP", "MXN", "ZAR", "BRL", "MYR", "RUB", "IDR", "INR", "KRW", "CNY",
+                "XDR"};
         currency.addAll(cr);
     }
 
